be858080bfcfc61fd42b9f2625eba0c0987061c4
ArielSanchez12 <riveraariel433@gmail.com>
2024-11-13 18:16:34 -0500

    Version Final

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,40 @@
-//Pongan sus nombres aquí: Ariel Sánchez
-//Integrantes: Patricio Ponce, Richard Padilla, Jose Pila
+//Integrantes: Patricio Ponce, Richard Padilla, Jose Pila, Ariel Sánchez
 import java.util.InputMismatchException;
-import java.util.MissingResourceException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("-----PLANTEAMIENTO DEL PROBLEMA-----\n" +
-                "Se patea un balón desde el suelo a un ángulo determinado y con una velocidad inicial." +
-                "\n¿Cuál es el alcance horizontal del balón?");
-        //Variables
-        int angulo=0, velocidad=0;
+                "Se patea un balon desde el suelo a un angulo determinado y con una velocidad inicial" +
+                "\n¿Cual es el alcance horizontal del balon?");
+
         //Instancia de clases
         Scanner ingresar = new Scanner(System. in);
         CalcularAlcanceH calculo1 = new CalcularAlcanceH();
 
         //INGRESO DE DATOS
-            try {
-                System.out.print("Ingrese la velocidad inicial del balon: ");
-                velocidad = ingresar.nextInt();
-                System.out.print("Ingrese el angulo de direccion: ");
-                angulo = ingresar.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.print("Datos ingresados incorrectamente....");
-            }
-        //OBJETO DONDE SE GUARDARA LOS RESULTADOS
-            calculo1.setAngulo((double)angulo);
-            calculo1.setVelocidadInicial((double)velocidad);
-            calculo1.VelocidadInicialX(velocidad,angulo);
-            calculo1.VelocidadInicialY(velocidad,angulo);
-        // PARTE DEL INSERTAR, AQUI...
+            boolean ingresarValido = false;
+            while (!ingresarValido) {
+                try {
+                    System.out.println("Ingrese la velocidad inicial del objeto: ");
+                    double velocidadInicial = ingresar.nextDouble();
+                    calculo1.setVelocidadInicial(velocidadInicial);
+                    System.out.println("Ingrese el angulo del objeto: ");
+                    double angulo = ingresar.nextDouble();
+                    calculo1.setAngulo(angulo);
 
+                    ingresarValido = true;
+                } catch (InputMismatchException e) {
+                    System.out.print("Datos ingresados incorrectamente, vuelve a ingresarlos\n");
+                    ingresar.nextLine();
+                }
+            }
+
+            //Imprimir resultados
+            System.out.println(calculo1.VelocidadInicialX(calculo1.getVelocidadInicial(), calculo1.getAngulo()));
+            System.out.println(calculo1.VelocidadInicialY(calculo1.getVelocidadInicial(), calculo1.getAngulo()));
+            System.out.println(calculo1.TiempoDeVuelo());
+            System.out.println(calculo1.AlcanceHorizontal());
 
     }
 }
